5a05ecfc15e090363d9cab8994c78181afd9f17f
Raja Shah <raja_shah2@cable.comcast.com>
2016-01-21 17:21:02 -0800

    ARRISXB3-4110 : Merge ccsp moca and platform hal changes from legacy to yocto
    Reason for change: Merge ccsp hal changes from legacy to yocto
    Test Procedure: None
    Risks: None
    Signed-off-by: Raja Shah <raja_shah2@cable.comcast.com>
    
    Change-Id: I3532c3e44361c91e3a594f614bf87d6ca78e4d38

--- source/platform/platform_hal.c
+++ source/platform/platform_hal.c
@@ -68,7 +68,7 @@ INT platform_hal_GetBaseMacAddress(CHAR *pValue) { strcpy(pValue, "BasMac"); ret
 INT platform_hal_GetHardware(CHAR *pValue) { strcpy(pValue, "Hard"); return RETURN_OK; }
 INT platform_hal_GetTotalMemorySize(ULONG *pulSize) { *pulSize = 512*1024; return RETURN_OK; }
 
-INT platform_hal_GetHardware_MemUsed(CHAR *pValue)
+INT platform_hal_GetHardware_MemUsed(CHAR *pValue) {*pValue='0'; return RETURN_OK;}
 {
     if (pValue == NULL)
     {
@@ -111,5 +111,20 @@ INT platform_hal_GetUsedMemorySize(ULONG *pulSize)
            return RETURN_ERR;
         }
         *pulSize = 0;
+        return RETURN_OK;
+}
+
+INT platform_hal_GetFactoryResetCount(ULONG *pulSize)
+{
+        if (pulSize == NULL)
+        {
+           return RETURN_ERR;
+        }
+        *pulSize = 2;
+        return RETURN_OK;
+}
+
+INT platform_hal_ClearResetCount(BOOLEAN bFlag)
+{
         return RETURN_OK;
 }
