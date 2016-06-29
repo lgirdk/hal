1aa8e9ae9327b6a14a34c58f81aa6f205f5480a4
Scott Angel <scott_angel@cable.comcast.com>
2016-06-29 17:11:14 +0000

    RDKB-6394: Move DPOE hals to generic
    Reason for change: The DPOE hals were only available in the Broadcom hals. These
    need to be moved to generic.
    
    Test Procedure: Verified that the hals exist in generic
    
    Risks: None.
    
    Change-Id: I80a67c4369747e4aee229f02b1079062c12b4a60
    Signed-off-by: scott_angel@cable.comcast.com

--- include/mso_mgmt_hal.h
+++ include/mso_mgmt_hal.h
@@ -81,5 +81,24 @@ mso_pwd_ret_status;
 */
 mso_pwd_ret_status mso_validatepwd(char *pwd);
 
+/* mso_set_pod_seed : */
+/**
+* Description: Sets the PoD seed for mso password validation,
+* Parameters: 
+*    CHAR* - PoD seed
+*
+* @return the status of the operation.
+* @returnval RETURN_OK if successful.
+* @returnval RETURN_ERR if any error is detected.
+*
+* @execution Synchronous.
+* @sideeffect None.
+*
+* @note This function must not suspend and must not invoke any blocking system 
+* calls. It should probably just send a message to a driver event handler task. 
+*
+*/
+INT mso_set_pod_seed(char *);
+
 #endif
 
