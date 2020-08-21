892595230f2168e74b0821f96479b8156d336e13
Rob Vogelaar <revogelaar@gmail.com>
2020-08-21 20:18:29 +0000

    wifi: drop references to wifi_getApIndexFromName()
    Although wifi_hal.h declares wifi_getApIndexFromName() (ie with "Ap")
    the HAL .c file and all callers use wifi_getIndexFromName() (ie
    without "Ap"). At some point wifi_getApIndexFromName() was added to
    the HAL .c file too, although nothing seems to be calling it.
    
    Fix the mismatch by removing wifi_getApIndexFromName() from the HAL
    .c file and updating wifi_hal.h to declare wifi_getIndexFromName().
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/wifi/wifi_hal.c
+++ source/wifi/wifi_hal.c
@@ -3315,11 +3315,6 @@ INT wifi_getBSSTransitionActivation(UINT apIndex, BOOL *activate)
     return RETURN_ERR;
 }
 
-INT wifi_getApIndexFromName(CHAR *inputSsidString, INT *output_int)
-{
-    return RETURN_ERR;
-}
-
 /**
  OPENSYNC WIFI HAL EXTENSIONS END
 */
