4d5d546e72292e00a982ad44e2f5b275d14cf82b
Andre McCurdy <armccurdy@gmail.com>
2021-10-28 13:03:09 -0700

    platform: add stub for platform_hal_SetLedMaxBrightness()
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/platform/platform_hal.c
+++ source/platform/platform_hal.c
@@ -571,6 +571,11 @@ int platform_hal_SetLedRegistrationErrorTimer(int value)
     return RETURN_OK;
 }
 
+int platform_hal_SetLedMaxBrightness(int value)
+{
+    return RETURN_OK;
+}
+
 int platform_hal_SetSpeedUpMTARFLossDetection(BOOLEAN bValue)
 {
     return RETURN_OK;
