315453728f9adcce948e891d7f64bfe20a1ecc1c
Andre McCurdy <armccurdy@gmail.com>
2021-11-23 16:50:17 -0800

    wifi: add stub for wifi_getApRadiusTransportInterface()
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/wifi/wifi_hal.c
+++ source/wifi/wifi_hal.c
@@ -2863,6 +2863,13 @@ INT wifi_setRadioChannelWeights (INT radioIndex, const ULONG *output_weights)
     return RETURN_OK;
 }
 
+INT wifi_getApRadiusTransportInterface (UINT *RadiusInterface)
+{
+    *RadiusInterface = 0;
+
+    return RETURN_OK;
+}
+
 INT wifi_setApRadiusTransportInterface (UINT RadiusInterface)
 {
     return RETURN_OK;
